8a0b4e791f1c5fbae22b94d96913d9c9451c87e6
maxusify <maxusify@gmail.com>
2022-02-03 01:12:47 +0100

    Update -- Screens init files

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* ====== AUTH SCREENS ====== */}
+        <Stack.Screen name="LoginScreen" component={Login} />
+        <Stack.Screen name="RegisterScreen" component={Register} />
+
+        {/* ====== HOME SCREEN ====== */}
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+
+        {/* ====== RESTAURANT SCREENS ====== */}
+        <Stack.Screen name="RestaurantsScreen" component={RestaurantList} />
+        <Stack.Screen name="RestaurantMapScreen" component={RestaurantList} />
+        <Stack.Screen name="FoodCategoriesScreen" component={FoodCategories} />
+        <Stack.Screen name="CartScreen" component={RestaurantList} />
+        <Stack.Screen name="Profile Screen" component={RestaurantList} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
